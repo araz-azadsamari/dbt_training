8af010d60f146f8fdf7a7f79abe8162a2c05f3a2
Araz Azadsamari <araz.admin@gmail.com>
2025-08-24 13:22:01 +0000

    added macros

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -6,9 +6,11 @@ o.ORDERSELLINGPRICE - o.ORDERCOSTPRICE as orderprofit
 --from raw_customer
 ,c.CUSTOMERID,c.CUSTOMERNAME,c.SEGMENT,c.COUNTRY
 --from raw_product
-,p.productid,p.CATEGORY,p.PRODUCTNAME,p.SUBCATEGORY
+,p.productid,p.CATEGORY,p.PRODUCTNAME,p.SUBCATEGORY,
+{{markup()}} as markup
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
 on o.CUSTOMERID=c.CUSTOMERID
 left join {{ ref('raw_product') }} as p
 on o.productid=p.productid
+{{limit_data_in_dev(orderdate)}}
